b9d64ab9f5a5d41851cef0d18ca57f633ae4560d
Patrick Hein <ThisIsIPat@users.noreply.github.com>
2020-08-19 10:29:46 +0200

    added next.js to dependencies

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const test = () => (
+    <p>
+      <Link href="/test">
+        <a>Nowhere</a>
+      </Link>
+    </p>
+);
+
+export default test;
